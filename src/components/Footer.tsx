bdb457ee11928c0b18172c7a54c66443266c66a3
Diógenes <123782296+diogenesdev7@users.noreply.github.com>
2024-02-01 16:42:28 -0300

    att footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 const Footer = () => {
   return (
     <div className='bg-black h-[60px] flex justify-center items-center'>
-        <Link target="_blank" href='https://github.com/diogenesdev7' className='text-[1rem]'> By diogenesdev7</Link>
+        <Link target="_blank" href='https://github.com/diogenesdev7/queststoremnext' className='text-[1rem]'> By diogenesdev7</Link>
     </div>
   )
 }
